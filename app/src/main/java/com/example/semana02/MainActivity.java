3f99fd917843edffb084a677486ab98b8822129d
7ionel <lionelorihuelac@gmail.com>
2024-04-13 10:48:48 -0500

    Avance 02

--- app/src/main/java/com/example/semana02/MainActivity.java
+++ app/src/main/java/com/example/semana02/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.semana02;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.example.semana02.adapter.UserAdapter;
+import com.example.semana02.entity.Address;
 import com.example.semana02.entity.User;
 import com.example.semana02.service.ServiceUser;
 import com.example.semana02.util.ConnectionRest;
@@ -26,91 +24,90 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner   spnUsuarios;
-    ArrayAdapter<String> adaptadorUsuarios;
-    ArrayList<String> listaUsuarios = new ArrayList<String>();
+    // ListView y Adapter
+    ListView lstUser;
+    ArrayList<User> listaUser = new ArrayList<User>();
+    UserAdapter userAdapter;
 
-    Button   btnFiltrar;
-    TextView txtResultado;
-
-    //conecta al servicio REST
+    // Conecta al servicio REST
     ServiceUser serviceUser;
 
     private List<User> listaTotalUsuarios;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        //Relaciona las variables con las variables de la GUI
-        adaptadorUsuarios = new ArrayAdapter<String>(
-                this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                listaUsuarios);
-        spnUsuarios = findViewById(R.id.spnUsuarios);
-        spnUsuarios.setAdapter(adaptadorUsuarios);
+        lstUser = findViewById(R.id.lstUsuarios);
+        userAdapter = new UserAdapter(this, R.layout.user_item, listaUser);
+        lstUser.setAdapter(userAdapter);
 
-        btnFiltrar = findViewById(R.id.btnFiltrar);
-        txtResultado = findViewById(R.id.txtResultado);
-
-        //Conecta al servicio REST
+        // Conecta al servicio REST
         serviceUser = ConnectionRest.getConnecion().create(ServiceUser.class);
 
-        cargaUsuarios();
-
-        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+        // Llama al método para cargar usuarios cuando se hace clic en el botón
+        findViewById(R.id.btnFiltrar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String item = spnUsuarios.getSelectedItem().toString();
-                int position = spnUsuarios.getSelectedItemPosition();
-                String id = item.split("-")[0];
-                String nombre = item.split("-")[1];
-
-                User objUserSeleccionado = listaTotalUsuarios.get(position);
-
-                String salida =  "Users: \n\n";
-                salida +=  "Position  " + position +"\n";
-                salida +=  "Id  " + id +"\n";
-                salida +=  "Name  " + nombre +"\n";
-                salida +=  "UserName  " + objUserSeleccionado.getUsername() +"\n";
-                salida +=  "Email  " + objUserSeleccionado.getEmail() +"\n";
-                salida +=  "Phone  " + objUserSeleccionado.getPhone() +"\n";
-
-                txtResultado.setText(salida);
-
+                cargaUsuarios();
             }
         });
 
-
+        // Establece clics de escucha para los elementos de la lista
+        lstUser.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtén el usuario seleccionado
+                User selectedUser = listaUser.get(position);
+                // Obtén la dirección del usuario seleccionado
+                Address address = selectedUser.getAddress();
+                // Muestra un mensaje con la información de la dirección
+                showMessageDialog("Dirección", getAddressInfo(address));
+            }
+        });
     }
 
-    void cargaUsuarios(){
+    // Método para cargar usuarios desde el servicio REST
+    void cargaUsuarios() {
         Call<List<User>> call = serviceUser.listausuarios();
         call.enqueue(new Callback<List<User>>() {
             @Override
             public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                   if (response.isSuccessful()){
-                       listaTotalUsuarios = response.body();
-                       for(User x:listaTotalUsuarios){
-                           listaUsuarios.add(x.getId() + " - " + x.getName());
-                       }
-                       adaptadorUsuarios.notifyDataSetChanged();
-                   }
+                if (response.isSuccessful()) {
+                    listaTotalUsuarios = response.body();
+                    listaUser.clear();
+                    listaUser.addAll(listaTotalUsuarios);
+                    userAdapter.notifyDataSetChanged();
+                }
             }
+
             @Override
             public void onFailure(Call<List<User>> call, Throwable t) {
-
+                // Manejar el error
             }
         });
-
     }
 
+    // Método para obtener la información de la dirección
+    private String getAddressInfo(Address address) {
+        return "Calle: " + address.getStreet() + "\n" +
+                "Suite: " + address.getSuite() + "\n" +
+                "Ciudad: " + address.getCity() + "\n" +
+                "Código Postal: " + address.getZipcode();
+    }
+
+    // Método para mostrar un diálogo con el título y el mensaje dados
+    private void showMessageDialog(String title, String message) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialogBuilder.show();
+    }
 }
